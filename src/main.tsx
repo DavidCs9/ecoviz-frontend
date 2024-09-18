d00721e4e801d57c395f7f3d987566fd5e653896
DavidCs9 <davidcastrosi@hotmail.com>
2024-09-18 12:21:44 -0600

    feat: Integrate Sentry for error tracking and performance monitoring in the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import posthog from "posthog-js";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://299586ea22c5520ddb6747f52a3426ec@o4507687306854400.ingest.us.sentry.io/4507975235534848",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", "https://0123543.xyz/calculate"],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 posthog.init("phc_OTgMmbM2VK6ovulBfEmABYtkWH2DFy4ey3zevORoNyb", {
   api_host: "https://us.i.posthog.com",
